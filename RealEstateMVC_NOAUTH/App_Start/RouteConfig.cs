150c7f49f6dd135422f9fa7864a6cd2917bbd433
Pawanyy <pawanyyy01@gmail.com>
2023-03-01 00:50:24 +0530

    addd rout attributes

--- RealEstateMVC_NOAUTH/App_Start/RouteConfig.cs
+++ RealEstateMVC_NOAUTH/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace RealEstateMVC_NOAUTH
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
