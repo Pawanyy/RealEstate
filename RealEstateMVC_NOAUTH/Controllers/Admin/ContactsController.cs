a3b477d574a8ce5a1c9ca849d0f1f47aad4da575
Pawanyy <pawanyyy01@gmail.com>
2023-03-01 18:20:07 +0530

    contacts list added

---
+++ RealEstateMVC_NOAUTH/Controllers/Admin/ContactsController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RealEstateMVC_NOAUTH.Models;
+
+namespace RealEstateMVC_NOAUTH.Controllers.Admin
+{
+    public class ContactsController : Controller
+    {
+        private RealEstateEntities db = new RealEstateEntities();
+
+        // GET: Contacts
+        public ActionResult Index()
+        {
+            return View(db.CONTACTs.ToList());
+        }
+
+        // GET: Contacts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CONTACT cONTACT = db.CONTACTs.Find(id);
+            if (cONTACT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cONTACT);
+        }
+
+        // POST: Contacts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CONTACT cONTACT = db.CONTACTs.Find(id);
+            db.CONTACTs.Remove(cONTACT);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
