3591ba9f9b7d8ecbafe4e1c4bba1d2e9082d6210
Pawanyy <pawanyyy01@gmail.com>
2023-03-01 18:00:05 +0530

    Property Status MetaData

---
+++ RealEstateMVC_NOAUTH/Models/PropertyStatusMetaData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace RealEstateMVC_NOAUTH.Models
+{
+    public class PropertyStatusMetaData
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Property Status")]
+        [StringLength(255, MinimumLength = 2)]
+        public string STATUS;
+    }
+}
